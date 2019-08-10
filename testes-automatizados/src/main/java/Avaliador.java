23d6c3f3b2633e8217b0f5c05eb601ba6abf5eaa
dogue100 <hugo.pereira.silva85@gmail.com>
2019-08-10 15:11:47 -0300

    Fixed the bug inside the code

--- testes-automatizados/src/main/java/Avaliador.java
+++ testes-automatizados/src/main/java/Avaliador.java
@@ -2,18 +2,18 @@ public class Avaliador {
     /*
     A var maiorDeTodos e inicializado com o menor valor possivel para um double,
     sendo assim ele cai na condiçao do if na primeira vez e é substituido pelo primeiro valor da lista
-    A var menorDeTodos segue a mesma condição porém no valor inverso BUG
+    A var menorDeTodos segue a mesma condição porém no valor inverso
     */
 
     private double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private double menorDeTodos = Double.POSITIVE_INFINITY;
 
     public void avalia(Leilao leilao){
-            for(Lance lance: leilao.getLances()){
+        for(Lance lance: leilao.getLances()){
             if(lance.getValor()> maiorDeTodos){
                 maiorDeTodos = lance.getValor();
             }
-            else if(lance.getValor()< menorDeTodos){
+            if(lance.getValor()< menorDeTodos){
                 menorDeTodos = lance.getValor();
             }
         }
--- testes-automatizados/src/main/java/Leilao.java
+++ testes-automatizados/src/main/java/Leilao.java
@@ -6,11 +6,11 @@ public class Leilao {
     private List<Lance> lances;
 
     public Leilao(String produto){
+        this.lances = new ArrayList<Lance>();
         this.produto = produto;
     }
 
     public void propoe(Lance lance){
-        this.lances = new ArrayList<Lance>();
         lances.add(lance);
     }
 
