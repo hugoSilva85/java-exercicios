a8193e7df5a89284f69072486cdaf641bfdd8f71
dogue100 <hugo.pereira.silva85@gmail.com>
2019-08-16 21:15:34 -0300

    Included a new class builder to help refactory testes

---
+++ testes-automatizados/src/test/java/CriadorDeLeilao.java
@@ -0,0 +1,16 @@
+public class CriadorDeLeilao {
+    private Leilao leilao;
+
+    public CriadorDeLeilao(){}
+
+    public CriadorDeLeilao para(String descricao){
+        this.leilao = new Leilao(descricao);
+        return this;
+    }
+
+    public CriadorDeLeilao lance(Usuario usuario, double valor){
+        leilao.propoe(new Lance(usuario,valor));
+        return this;
+    }
+
+}
