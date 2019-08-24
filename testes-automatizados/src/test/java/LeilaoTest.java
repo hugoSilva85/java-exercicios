a9b5e5f6aab7ae72d4ef0783b679a67fd25bec73
dogue100 <hugo.pereira.silva85@gmail.com>
2019-08-24 11:13:55 -0300

    Included tests to Leilao class to follow TDD Chapter

---
+++ testes-automatizados/src/test/java/LeilaoTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LeilaoTest {
+    private Usuario yuri;
+    private Usuario hugo;
+    private Usuario tete;
+
+    private void criaAvaliador() {
+        this.yuri = new Usuario("Yuri");
+        this.hugo = new Usuario("Hugo");
+        this.tete = new Usuario("Tete");
+    }
+
+    @Test
+    public void GivenIamReceivedJustOneBid() {
+        Leilao leilao = new CriadorDeLeilao()
+                .para("Carro com sinistro 2016")
+                .lance(yuri, 3000.0)
+                .constori();
+
+        assertThat(1).isEqualTo(leilao.getLances().size());
+    }
+
+    @Test
+    public void GivenIamReceivedManyBids() {
+        Leilao leilao = new CriadorDeLeilao()
+                .para("Carro com sinistro 2016")
+                .lance(yuri, 3000.0)
+                .lance(tete, 3500.0)
+                .lance(hugo, 5000.0)
+                .constori();
+
+        assertThat(3).isEqualTo(leilao.getLances().size());
+    }
+}
+
