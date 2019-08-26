4418671a6e982fffdb74fc0381a38550bed87234
dogue100 <hugo.pereira.silva85@gmail.com>
2019-08-25 21:31:09 -0300

    Exclude import not used

--- testes-automatizados/src/main/java/Lance.java
+++ testes-automatizados/src/main/java/Lance.java
@@ -1,10 +1,7 @@
-import org.jetbrains.annotations.Contract;
-
 public class Lance {
     private Usuario usuario;
     private double valor;
 
-    @Contract(pure = true)
     public Lance(Usuario usuario, double valor){
         this.usuario = usuario;
         this.valor = valor;
